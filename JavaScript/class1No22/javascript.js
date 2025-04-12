d1f5c2939ce578e131a646853e9303f4f2c79148
Maknoon Zia <maknoonzia13@gmail.com>
2025-04-12 14:36:04 +0500

    Js Class

---
+++ JavaScript/class1No22/javascript.js
@@ -0,0 +1,41 @@
+// name ---> Alphabet
+// age ----> Number
+// weight ---> Float 
+// true 1 , false 0
+
+// Data Types 
+// string  --> alphabet, characters
+// integer --> number  1, 2 ,3 ,4 
+// float --> decimal numbers 0.1, 8.5
+
+
+// variables-names --> x, y, z
+// 0010 --> x
+// 0011 --> y
+
+// name = 'fatima'
+// x=2
+// y = 10 
+// z = x + y
+
+
+first_name = 'Noor'
+last_name = "Fatima"
+age = 10
+weight = 4.0
+address = "Syllani house no 2"
+
+// for print 
+console.log(first_name, last_name)
+console.log("Age:", age , "Weight:", weight)
+console.log(address)
+
+// simple console msg
+console.log("Hello World")
+
+// operation in console
+firstValue = 10
+secondValue= 20
+console.log(firstValue+secondValue)
+result = firstValue+ secondValue
+console.log("result", result)
---
+++ JavaScript/classNo24/javascript.js
@@ -0,0 +1,54 @@
+// integer, number, string, null ,boolean
+// var , let , cnst
+
+
+
+// const pi = 3.14
+// console.log(pi)
+// let username = 'fatima'
+// username = 'noor'
+// console.log(username)
+// var roll_no = 25
+// var roll_no = 36
+// console.log(roll_no)
+// document.getElementById('new').innerHTML=("<h1>Hello</h1>")
+
+
+
+// const x = 10, y=20 ------> ERROR
+// x = 10
+// x = x +20
+// document.getElementById("first").innerText =(x)
+// x += 20
+// console.log("Addition assignment-->", x)
+// x -= 20
+// console.log("Subtract assignment-->", x)
+// x *= 20
+// console.log("Multiplictaion assignment-->", x)
+// x /= 20
+// console.log("Division assignment-->", x)
+// document.getElementById("second").innerText =(x)
+
+
+// first_value = 200
+// second_value = 100
+// console.log(first_value > second_value)
+// console.log(first_value < second_value)
+// console.log(first_value >= second_value)
+// console.log(first_value <= second_value)
+// console.log(first_value != second_value)
+// check_greater = first_value > second_value
+// chech_less_than = first_value < second_value
+// console.log(check_greater && chech_less_than)
+// console.log( (20 == 10) || (200 > 100))
+// console.log( !(20 == 20))
+
+// data type doesn't matter
+console.log( 10 == "10")
+
+// check data type with ===
+console.log( 10 === "10")
+
+// DATA TYPE NT CHECK
+console.log( 10 != "10")
+
